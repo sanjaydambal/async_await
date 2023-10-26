f02b92c6f93d51ae9feb902528eff441ede54847
sanjaydambal <sanjaydambal@gmail.com>
2023-10-26 21:19:04 +0530

    setInterval

--- setTimout.js
+++ setTimout.js
@@ -2,7 +2,13 @@ const fn = ()=>{
     console.log("hello")
 }
 setTimeout(fn,2000);
+
 const fn1 = (name)=>{
     console.log(`hello ${name}`)
 }
 clearTimeout(setTimeout(fn1,2000,"sanjay"));
+
+const fn2 = (name)=>{
+    console.log(`hello ${name}`)
+}
+setInterval(fn1,2000,"sanjay");
